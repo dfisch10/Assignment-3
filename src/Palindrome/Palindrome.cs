1af422d4fddb392ba426238289c4718a8a8dbd7b
Daniel Fischer <60206685+dfisch10@users.noreply.github.com>
2020-06-15 17:23:43 -0600

    Add files via upload

--- src/Palindrome/Palindrome.cs
+++ src/Palindrome/Palindrome.cs
@@ -1,4 +1,8 @@
-﻿namespace Palindrome
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Palindrome
 {
     /// <summary>
     ///     Palindrome class is responsible for determining if an input is equal to a palindrome. 
@@ -8,15 +12,37 @@
         #region Methods
 
         /// <summary>
-        ///     Is valid checks if a string (word or phrase) input is truely a panindrome.
+        ///     Is valid checks if a string (word or phrase) input is truely a palindrome.
         /// </summary>
         /// <param name="input">The input to validate.</param>
         /// <returns>If the input is a valid palindrome.</returns>
         public bool IsValid(string input)
         {
-            return false;
-        }
+            if (input is null)
+            {
+                return false;
+            }
+            else
+            {
+                input = Regex.Replace(input, @"[^0-9a-zA-Z]+", "");
+                input = input.ToLower();
 
+                char[] charArray = input.ToCharArray();
+
+                Array.Reverse(charArray);
+
+                string reverseInput = new string(charArray);
+
+                if (input != reverseInput)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            } 
+        }
         #endregion
     }
 }
